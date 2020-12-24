9d5f5fad4a8f04475fcc9bf885ff2cc049e4cfa9
kontrolbits <gabodecampos@gmail.com>
2020-12-24 15:54:14 -0300

    update

---
+++ src/components/Card.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="col-12 q-ml-xs q-mr-md q-col-gutter-md row">
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+    :href="link"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>
+        {{ caption }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+        <div class="col-md-3 col-sm-12 col-xs-12">
+            <!-- card1 -->
+            <q-card bordered>
+                <q-card-section>
+                    <div class="row items-center no-wrap">
+                        <div class="col q-pl-sm">
+                            <div class="text-h6">Our Planet</div>
+                            <div class="text-subtitle2">by John Doe</div>
+                        </div>
+                        <div class="col-auto">
+                            <q-btn color="grey-7" round flat icon="more_vert">
+                                <q-menu cover auto-close>
+                                    <q-list>
+                                        <q-item clickable>
+                                            <q-item-section>Remove Card</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Send Feedback</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Share</q-item-section>
+                                        </q-item>
+                                    </q-list>
+                                </q-menu>
+                            </q-btn>
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-card-section>
+                    {{ lorem }}
+                </q-card-section>
+
+                <q-separator />
+
+                <q-card-actions>
+                    <q-btn flat>Action 1</q-btn>
+                    <q-btn flat>Action 2</q-btn>
+                </q-card-actions>
+            </q-card>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    caption: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: '#'
+    },
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      dataGrafico: 'dia'
+    }
+  }
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,49 +1,21 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          KB Gestor
-        </q-toolbar-title>
-
-        <div></div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
-
+<q-page>
+    <div class="col-12 q-pa-md">
+        <q-breadcrumbs style="font-size: 15px">
+            <q-breadcrumbs-el label="Home" />
+        </q-breadcrumbs>
+    </div>
     <q-page-container>
-      <router-view />
+      <Card v-for="link in essentialLinks" :key="link.title" v-bind="link"/>
     </q-page-container>
-  </q-layout>
+</q-page>
 </template>
 
+<style>
+</style>
+
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+import Card from 'components/Card.vue'
 
 const linksData = [
   {
@@ -91,11 +63,12 @@ const linksData = [
 ]
 
 export default {
-  name: 'MainLayout',
-  components: { EssentialLink },
+  name: 'home',
+  components: { Card },
   data () {
     return {
-      leftDrawerOpen: false,
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      dataGrafico: 'dia',
       essentialLinks: linksData
     }
   }
